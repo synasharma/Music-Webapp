8d6133c4059cbd48c616aa9009ca8a1c7282c820
synasharma <61623971+synasharma@users.noreply.github.com>
2021-05-13 21:49:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song_1="";
+song_2="";
+
+function preload()
+{
+song_1=loadSound("runaway.mp3");
+song_2=loadSound("conqueror.mp3");
+}
+
+function setup()
+{
+canvas=createCanvas(500,400);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw()
+{
+image(video,0,0,500,400)
+}
